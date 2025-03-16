4c09f0aa0da796be976b429fab28ac486cc425e4
elder1205 <elder.solano@sofka.com.co>
2025-03-16 14:41:16 -0500

    Delete import Optional

--- account-movements-microservices/infrastructure/handler-adapters/mysql/src/main/java/ec.com.efsr/repository/IAccountJpaRepository.java
+++ account-movements-microservices/infrastructure/handler-adapters/mysql/src/main/java/ec.com.efsr/repository/IAccountJpaRepository.java
@@ -3,8 +3,6 @@ package ec.com.efsr.repository;
 import ec.com.efsr.entities.AccountEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface IAccountJpaRepository extends JpaRepository<AccountEntity, String> {
     AccountEntity findByAccountNumber(String accountNumber);
     AccountEntity findByIdCustomer(String idCustomer);
